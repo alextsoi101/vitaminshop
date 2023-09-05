acb889618220446c5c77e43c92dd6c230e05d16f
Alex Tsoi <alextsoi101@gmail.com>
2023-09-04 22:08:41 -0500

    Fix database ssl error

--- server/db/db.js
+++ server/db/db.js
@@ -5,8 +5,13 @@ module.exports = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    dialect: 'postgres',
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
   }
 );
