c979b699c30355c7323b6ea13886364595e35a32
Alex Tsoi <alextsoi101@gmail.com>
2023-09-21 10:57:31 -0500

    Update admin router

--- client/src/routes/AdminRouter.jsx
+++ client/src/routes/AdminRouter.jsx
@@ -3,6 +3,11 @@ import { Routes, Route, Outlet, Navigate } from 'react-router-dom';
 import AdminHeader from '../adminpanel/layout/AdminHeader';
 import AdminNavBar from '../adminpanel/layout/AdminNavBar';
 import DashboardPage from '../adminpanel/pages/DashboardPage';
+import ProductListPage from '../adminpanel/pages/ProductListPage';
+import UserListPage from '../adminpanel/pages/UserListPage';
+import PromoListPage from '../adminpanel/pages/PromoListPage';
+import OrderListPage from '../adminpanel/pages/OrderListPage';
+import CategoryListPage from '../adminpanel/pages/CategoryListPage';
 import '../adminpanel/styles/globalstyles/adminpanel.scss';
 
 const AdminRouter = () => {
@@ -25,6 +30,11 @@ const AdminRouter = () => {
         }
       >
         <Route path='/' element={<DashboardPage/>} />
+        <Route path='products' element={<ProductListPage/>} />
+        <Route path='users' element={<UserListPage/>} />
+        <Route path='promocodes' element={<PromoListPage/>} />
+        <Route path='orders' element={<OrderListPage/>} />
+        <Route path='categories' element={<CategoryListPage/>} />
         <Route
           path="*"
           element={<Navigate to="/admin" replace />}
