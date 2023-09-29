6e8b840bfdf8fa0ffd7cc1078f725b403f284d8a
Alex Tsoi <alextsoi101@gmail.com>
2023-09-29 00:15:06 -0500

    Update Order router

--- server/routes/orderRouter.js
+++ server/routes/orderRouter.js
@@ -4,6 +4,12 @@ const router = new Router();
 const orderController = require('../controllers/orderController');
 
 router.post('/', orderController.create);
-router.get('/:userId', orderController.getAll);
+router.get('/all', orderController.getAll);
+router.get('/one/:id', orderController.getOne);
+router.get('/user/:userId', orderController.getAllUserOrders);
+router.get('/orderstatistic', orderController.getOrderStatistic);
+router.get('/salestatistic', orderController.getSaleStatistic);
+router.get('/lifetimeorders', orderController.getLifetimeOrders);
+router.get('/lifetimesales', orderController.getLifetimeSales);
 
 module.exports = router;
