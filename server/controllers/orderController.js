9ce669a56bb432b5812063600826f6dad3cd9e04
Alex Tsoi <alextsoi101@gmail.com>
2023-09-29 00:14:22 -0500

    Add server functions for Order controllers

--- server/controllers/orderController.js
+++ server/controllers/orderController.js
@@ -67,6 +67,31 @@ class OrderController {
   }
 
   async getAll(req, res, next) {
+    let {id, email, limit, page} = req.query
+
+    page = page || 1;
+    let offset = page * limit - limit
+
+    try {
+      const ordersWithProducts = await OrderService.getAll(id, email, limit, offset);
+      return res.json(ordersWithProducts)
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  async getOne(req, res, next) {
+    let {id} = req.params
+
+    try {
+      const orderWithProducts = await OrderService.getOne(id);
+      return res.json(orderWithProducts)
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  async getAllUserOrders(req, res, next) {
     let {userId} = req.params
 
     if (!userId) {
@@ -74,8 +99,64 @@ class OrderController {
     }
 
     try {
-      const ordersWithProducts = await OrderService.getAllOrders(userId);
+      const ordersWithProducts = await OrderService.getAllUserOrders(userId);
       return res.json(ordersWithProducts)
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  async getOrderStatistic(req, res, next) {
+    let {startDate, lastDate} = req.query
+
+    if (!startDate) {
+      return next(ApiError.internal('Start day cannot be empty'))
+    }
+
+    if (!lastDate) {
+      return next(ApiError.internal('Last day cannot be empty'))
+    }
+
+    try {
+      const orderStatistic = await OrderService.getOrderStatistic(startDate, lastDate);
+      return res.json(orderStatistic)
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  async getSaleStatistic(req, res, next) {
+    let {startDate, lastDate} = req.query
+
+    if (!startDate) {
+      return next(ApiError.internal('Start day cannot be empty'))
+    }
+
+    if (!lastDate) {
+      return next(ApiError.internal('Last day cannot be empty'))
+    }
+
+    try {
+      const saleStatistic = await OrderService.getSaleStatistic(startDate, lastDate);
+      return res.json(saleStatistic)
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  async getLifetimeSales(req, res, next) {
+    try {
+      const lifetimeSales = await OrderService.getLifetimeSales();
+      return res.json(lifetimeSales)
+    } catch (error) {
+      return next(error)
+    }
+  }
+
+  async getLifetimeOrders(req, res, next) {
+    try {
+      const lifetimeOrders = await OrderService.getLifetimeOrders();
+      return res.json(lifetimeOrders)
     } catch (error) {
       return next(error)
     }
--- server/services/order-service.js
+++ server/services/order-service.js
@@ -1,4 +1,5 @@
 const {Order, Cart, CartProduct, Product, OrderedProduct} = require('../db/models/models');
+const { Op, fn, col } = require("sequelize");
 
 class OrderService {
 
@@ -114,7 +115,60 @@ class OrderService {
     return orderWithProducts;
   }
 
-  async getAllOrders(userId) {
+  async getAll(id, email, limit, offset) {
+
+    if (!id && !email) {
+      const orderWithProducts = await Order.findAndCountAll({
+        order: [['id', 'DESC']], limit, offset
+      })
+      return orderWithProducts;
+    }
+
+    if (id && !email) {
+      const orderWithProducts = await Order.findAndCountAll({
+        where: {id},
+        order: [['id', 'DESC']], limit, offset
+      })
+      return orderWithProducts;
+    }
+
+    if (!id && email) {
+      const orderWithProducts = await Order.findAndCountAll({
+        where: {email},
+        order: [['id', 'DESC']], limit, offset
+      })
+      return orderWithProducts;
+    }
+
+    if (id && email) {
+      const orderWithProducts = await Order.findAndCountAll({
+        where: {
+          [Op.or]: [
+            {id: id},
+            {email: email},
+          ]
+        },
+        order: [['id', 'DESC']], limit, offset
+      })
+      return orderWithProducts;
+    }
+  }
+
+  async getOne(id) {
+    const orderWithProducts = await Order.findOne({
+      where: {id},
+      include: [{
+        model: OrderedProduct,
+        as: 'orderedProducts',
+        include: [{
+          model: Product
+        }]
+      }]
+    })
+    return orderWithProducts;
+  }
+
+  async getAllUserOrders(userId) {
     const orderWithProducts = await Order.findAll({
       where: {userId},
       include: [{
@@ -126,6 +180,143 @@ class OrderService {
       }]
     })
     return orderWithProducts;
+  }
+
+  async getOrderStatistic(startDate, lastDate) {
+
+    try {
+      const orderStatistic = await Order.findAll({
+        where: {
+          createdAt: { [Op.between]: [new Date(startDate), new Date(lastDate)] },
+        },
+        attributes: [
+          'createdAt', 
+          [fn('date_trunc', 'day', col('createdAt')), 'orderDay'],
+          [fn('COUNT', col('id')), 'orderCount']
+        ],
+        group: 'createdAt',
+        raw: true,
+        right: false,
+        order: [['createdAt', 'ASC']],
+      })
+
+      const orderCountMap = new Map();
+      orderStatistic.forEach((row) => {
+        orderCountMap.set(row.orderDay.toISOString().split('T')[0], row.orderCount);
+      });
+
+      const generateDateArray = (startDay, lastDay) => {
+        const dates = [];
+        const startDate = new Date(startDay);
+        const lastDate = new Date(lastDay);
+      
+        while (startDate <= lastDate) {
+          dates.push(new Date(startDate));
+          startDate.setDate(startDate.getDate() + 1);
+        }
+      
+        return dates;
+      }
+
+      const dates = generateDateArray(startDate, lastDate)
+
+      const orderCounts = dates.map((date) => ({
+        date: date.toISOString().split('T')[0],
+        count: orderCountMap.get(date.toISOString().split('T')[0]) || 0,
+      }));
+    
+      return orderCounts;
+    }
+    catch (e) {
+      console.error(e)
+    }
+  }
+
+  async getSaleStatistic(startDate, lastDate) {
+
+    try {
+      const orderStatistic = await Order.findAll({
+        where: {
+          createdAt: { [Op.between]: [new Date(startDate), new Date(lastDate)] },
+        },
+        attributes: [
+          'createdAt', 
+          [fn('date_trunc', 'day', col('createdAt')), 'saleDay'],
+          [fn('sum', col('total')), 'total']
+        ],
+        group: 'createdAt',
+        raw: true,
+        right: false,
+        order: [['createdAt', 'ASC']],
+      })
+
+      const orderCountMap = new Map();
+      orderStatistic.forEach((row) => {
+        orderCountMap.set(row.saleDay.toISOString().split('T')[0], row.total);
+      });
+
+      const generateDateArray = (startDay, lastDay) => {
+        const dates = [];
+        const startDate = new Date(startDay);
+        const lastDate = new Date(lastDay);
+      
+        while (startDate <= lastDate) {
+          dates.push(new Date(startDate));
+          startDate.setDate(startDate.getDate() + 1);
+        }
+      
+        return dates;
+      }
+
+      const dates = generateDateArray(startDate, lastDate)
+
+      const salesByDay = dates.map((date) => ({
+        date: date.toISOString().split('T')[0],
+        total: orderCountMap.get(date.toISOString().split('T')[0]) || 0,
+      }));
+    
+      return salesByDay;
+    }
+    catch (e) {
+      console.error(e)
+    }
+  }
+
+  async getLifetimeOrders() {
+    const authOrders = await Order.findAll({
+      where: {
+        userId: {[Op.ne]: null} 
+      },
+      attributes: [
+        [fn('COUNT', col('id')), 'orders']
+      ],
+      raw: true
+    })
+
+    const nonAuthOrders = await Order.findAll({
+      where: {
+        userId: {[Op.eq]: null} 
+      },
+      attributes: [
+        [fn('COUNT', col('id')), 'orders']
+      ],
+      raw: true
+    })
+
+    return {
+      authOrders: authOrders[0].orders,
+      nonAuthOrders: nonAuthOrders[0].orders
+    };
+  }
+  
+  async getLifetimeSales() {
+    const lifetimeSales = await Order.findAll({
+      attributes: [
+        [fn('sum', col('total')), 'total']
+      ],
+    })
+
+    return lifetimeSales;
   }
 }
 
