f0b51358c1cab2668965fc1579d4b6ff622bc22f
Alex Tsoi <alextsoi101@gmail.com>
2023-10-05 09:10:18 -0500

    Add admin pagination component

---
+++ client/src/adminpanel/components/common/AdminPagination.jsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from "react";
+import Pagination from '@mui/material/Pagination';
+
+const AdminPagination = (props) => {
+
+  const [count, setCount] = useState(1);
+
+  useEffect(() => {
+    setCount(Number(props.pageCount))
+  }, [props.pageCount])
+
+  const handleChange = (event, value) => {
+    if (count > 1) {
+      props.onChange(value)
+    }
+  }
+
+  return (
+    <div className="userlistpage">
+      {count > 0 &&
+        <Pagination 
+        size="medium"
+        shape="rounded"
+        variant="outlined"
+        count={count}
+        onChange={handleChange}
+      />
+      }
+    </div>
+  )
+}
+
+export default AdminPagination;
