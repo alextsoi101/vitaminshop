bc3727e0120fce2401389111625469ad9c0d7797
Alex Tsoi <alextsoi101@gmail.com>
2023-10-05 23:14:36 -0500

    Update admin navbar

--- client/src/adminpanel/layout/AdminNavBar.jsx
+++ client/src/adminpanel/layout/AdminNavBar.jsx
@@ -6,6 +6,8 @@ import BallotIcon from '@mui/icons-material/Ballot';
 import InventoryIcon from '@mui/icons-material/Inventory';
 import AddBoxIcon from '@mui/icons-material/AddBox';
 import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import DiscountIcon from '@mui/icons-material/Discount';
+import CategoryIcon from '@mui/icons-material/Category';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 
 const AdminNavBar = () => {
@@ -19,19 +21,14 @@ const AdminNavBar = () => {
     navigate('dashboard')
   }
 
-  const goToOrders = () => {
+  const goToOrderList = () => {
     setCurrentLocation('/admin/orders')
     navigate('orders')
   }
 
-  const goToProducts = () => {
+  const goToProductList = () => {
     setCurrentLocation('/admin/products')
     navigate('products')
-  }
-
-  const goToAddProduct = () => {
-    setCurrentLocation('/admin/products/new')
-    navigate('products/new')
   }
 
   const goToUserList = () => {
@@ -39,104 +36,192 @@ const AdminNavBar = () => {
     navigate('users')
   }
 
+  const goToPromocodeList = () => {
+    setCurrentLocation('/admin/promocodes')
+    navigate('promocodes')
+  }
+
+  const goToCategoryList = () => {
+    setCurrentLocation('/admin/categories')
+    navigate('categories')
+  }
+
+  const goToAddProduct = () => {
+    setCurrentLocation('/admin/products/new')
+    navigate('products/new')
+  }
+
+  const goToAddPromocode = () => {
+    setCurrentLocation('/admin/promocodes/new')
+    navigate('promocodes/new')
+  }
+
   const handleLogout = () => {
     setCurrentLocation('signout')
-    navigate('/')
+    navigate('users')
   }
 
   return (
     <div className="adminnavbar">
       <ul className="adminnavbar-list">
-        <li
-          className="list-item"
-          onClick={goToDashboard}
-        >
-          <div 
-            className={currentLocation === '/admin' ? "left-mark" : ""}
+        <div className="adminnavbar-block">
+          <div className="block-main-text">
+            STATISTIC
+          </div>
+          <li
+            className="list-item"
+            onClick={goToDashboard}
           >
+            <div 
+              className={currentLocation === '/admin' ? "left-mark" : ""}
+            >
+            </div>
+            <div className={currentLocation === '/admin' ? "item-content-active" : "item-content"}>
+              <div className="icon-wrapper">
+                <DashboardIcon fontSize='24' />
+              </div> 
+              <span>Dashboard</span>
+            </div>
+          </li>
+        </div>
+
+        <div className="adminnavbar-block">
+          <div className="block-main-text">
+            CATALOG
           </div>
-          <div className={currentLocation === '/admin' ? "item-content-active" : "item-content"}>
-            <div className="icon-wrapper">
-              <DashboardIcon fontSize='24' />
-            </div> 
-            <span>Dashboard</span>
-          </div>
-        </li>
-        <li
-          className="list-item"
-          onClick={goToOrders}
-        >
-          <div 
-            className={currentLocation === '/admin/orders' ? "left-mark" : ""}
+          <li
+            className="list-item"
+            onClick={goToProductList}
           >
-          </div>
-          <div className={currentLocation === '/admin/orders' ? "item-content-active" : "item-content"}>
-            <div className="icon-wrapper">
-              <BallotIcon fontSize='24' />
-            </div> 
-            <span>Orders</span>
-          </div>
-        </li>
-        <li
-          className="list-item"
-          onClick={goToProducts}
-        >
-          <div 
-            className={currentLocation === '/admin/products' ? "left-mark" : ""}
+            <div 
+              className={currentLocation === '/admin/products' ? "left-mark" : ""}
+            >
+            </div>
+            <div className={currentLocation === '/admin/products' ? "item-content-active" : "item-content"}>
+              <div className="icon-wrapper">
+                <InventoryIcon fontSize='24' />
+              </div> 
+              <span>Products</span>
+            </div>
+          </li>
+          <li
+            className="list-item"
+            onClick={goToCategoryList}
           >
-          </div>
-          <div className={currentLocation === '/admin/products' ? "item-content-active" : "item-content"}>
-            <div className="icon-wrapper">
-              <InventoryIcon fontSize='24' />
-            </div> 
-            <span>Products</span>
-          </div>
-        </li>
-        <li
-          className="list-item"
-          onClick={goToAddProduct}
-        >
-          <div 
-            className={currentLocation === '/admin/products/new' ? "left-mark" : ""}
+            <div 
+              className={currentLocation === '/admin/categories' ? "left-mark" : ""}
+            >
+            </div>
+            <div className={currentLocation === '/admin/categories' ? "item-content-active" : "item-content"}>
+              <div className="icon-wrapper">
+                <CategoryIcon fontSize='24' />
+              </div> 
+              <span>Categories</span>
+            </div>
+          </li>
+          <li
+            className="list-item"
+            onClick={goToPromocodeList}
           >
-          </div>
-          <div className={currentLocation === '/admin/products/new' ? "item-content-active" : "item-content"}>
-            <div className="icon-wrapper">
-              <AddBoxIcon fontSize='24' />
-            </div> 
-            <span>New Product</span>
-          </div>
-        </li>
-        <li
-          className="list-item"
-          onClick={goToUserList}
-        >
-          <div 
-            className={currentLocation === '/admin/users' ? "left-mark" : ""}
+            <div 
+              className={currentLocation === '/admin/promocodes' ? "left-mark" : ""}
+            >
+            </div>
+            <div className={currentLocation === '/admin/promocodes' ? "item-content-active" : "item-content"}>
+              <div className="icon-wrapper">
+                <DiscountIcon fontSize='24' />
+              </div> 
+              <span>Promocodes</span>
+            </div>
+          </li>
+          <li
+            className="list-item"
+            onClick={goToOrderList}
           >
-          </div>
-          <div className={currentLocation === '/admin/users' ? "item-content-active" : "item-content"}>
-            <div className="icon-wrapper">
-              <PeopleAltIcon fontSize='24' />
-            </div> 
-            <span>Users</span>
-          </div>
-        </li>
-        <li
-          className="list-item"
-          onClick={handleLogout}
-        >
-          <div 
-            className={currentLocation === 'logout' ? "left-mark" : ""}
+            <div 
+              className={currentLocation === '/admin/orders' ? "left-mark" : ""}
+            >
+            </div>
+            <div className={currentLocation === '/admin/orders' ? "item-content-active" : "item-content"}>
+              <div className="icon-wrapper">
+                <BallotIcon fontSize='24' />
+              </div> 
+              <span>Orders</span>
+            </div>
+          </li>
+          <li
+            className="list-item"
+            onClick={goToUserList}
           >
+            <div 
+              className={currentLocation === '/admin/users' ? "left-mark" : ""}
+            >
+            </div>
+            <div className={currentLocation === '/admin/users' ? "item-content-active" : "item-content"}>
+              <div className="icon-wrapper">
+                <PeopleAltIcon fontSize='24' />
+              </div> 
+              <span>Users</span>
+            </div>
+          </li>
+        </div>
+
+        <div className="adminnavbar-block">
+          <div className="block-main-text">
+            CREATE
           </div>
-          <div className={currentLocation === 'logout' ? "item-content-active" : "item-content"}>
-            <div className="icon-wrapper">
-              <ExitToAppIcon fontSize='24' />
-            </div> 
-            <span>Logout</span>
+          <li
+            className="list-item"
+            onClick={goToAddProduct}
+          >
+            <div 
+              className={currentLocation === '/admin/products/new' ? "left-mark" : ""}
+            >
+            </div>
+            <div className={currentLocation === '/admin/products/new' ? "item-content-active" : "item-content"}>
+              <div className="icon-wrapper">
+                <AddBoxIcon fontSize='24' />
+              </div> 
+              <span>New Product</span>
+            </div>
+          </li>
+          <li
+            className="list-item"
+            onClick={goToAddPromocode}
+          >
+            <div 
+              className={currentLocation === '/admin/promocodes/new' ? "left-mark" : ""}
+            >
+            </div>
+            <div className={currentLocation === '/admin/promocodes/new' ? "item-content-active" : "item-content"}>
+              <div className="icon-wrapper">
+                <AddBoxIcon fontSize='24' />
+              </div> 
+              <span>New Promocode</span>
+            </div>
+          </li>
+        </div>
+
+        <div className="adminnavbar-block">
+          <div className="block-main-text">
+            LOGOUT
           </div>
-        </li>
+          <li
+            className="list-item"
+            onClick={handleLogout}
+          >
+            <div 
+              className={currentLocation === 'logout' ? "left-mark" : ""}
+            >
+            </div>
+            <div className={currentLocation === 'logout' ? "item-content-active" : "item-content"}>
+              <div className="icon-wrapper">
+                <ExitToAppIcon fontSize='24' />
+              </div> 
+              <span>Logout</span>
+            </div>
+          </li>
+        </div>
       </ul>
     </div>
   )
