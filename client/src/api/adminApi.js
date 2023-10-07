3dea3688dc480896955e6616e405369c114905fe
Alex Tsoi <alextsoi101@gmail.com>
2023-10-06 22:41:37 -0500

    Update client admin api

--- client/src/api/adminApi.js
+++ client/src/api/adminApi.js
@@ -14,68 +14,68 @@ export const adminCheck = async () => {
 }
 
 export const fetchOrderStatistic = async (startDate, lastDate) => {
-  const {data} = await $host.get('api/order/orderstatistic', {params: {startDate, lastDate}})
+  const {data} = await $authAdminHost.get('api/order/orderstatistic', {params: {startDate, lastDate}})
   return data
 }
 
 export const fetchSaleStatistic = async (startDate, lastDate) => {
-  const {data} = await $host.get('api/order/salestatistic', {params: {startDate, lastDate}})
+  const {data} = await $authAdminHost.get('api/order/salestatistic', {params: {startDate, lastDate}})
   return data
 }
 
 export const fetchLifetimeOrders = async () => {
-  const {data} = await $host.get('api/order/lifetimeorders')
+  const {data} = await $authAdminHost.get('api/order/lifetimeorders')
   return data
 }
 
 export const fetchLifetimeSales = async () => {
-  const {data} = await $host.get('api/order/lifetimesales')
+  const {data} = await $authAdminHost.get('api/order/lifetimesales')
   return data
 }
 
 export const fetchUserStatistic = async (startDate, lastDate) => {
-  const {data} = await $host.get('api/user/userstatistic', {params: {startDate, lastDate}})
+  const {data} = await $authAdminHost.get('api/user/userstatistic', {params: {startDate, lastDate}})
   return data
 }
 
 export const fetchUsers = async (userId, name, email, limit, page) => {
-  const {data} = await $host.get('api/user/all', {params: {
+  const {data} = await $authAdminHost.get('api/user/all', {params: {
     userId, name, email, limit, page
   }})
   return data
 }
 
 export const fetchUserInfo = async (userId) => {
-  const {data} = await $host.get('api/user/info/' + userId)
+  const {data} = await $authAdminHost.get('api/user/info/' + userId)
   return data
 }
 
 export const fetchUserOrders = async (userId) => {
-  const {data} = await $host.get('api/order/user/' + userId)
+  const {data} = await $authAdminHost.get('api/order/user/' + userId)
   return data
 }
 
 export const fetchOrders = async (id, email, limit, page) => {
-  const {data} = await $host.get('api/order/all', {params: {
+  const {data} = await $authAdminHost.get('api/order/all', {params: {
     id, email, limit, page
   }})
   return data
 }
 
 export const fetchOrderInfo = async (id) => {
-  const {data} = await $host.get('api/order/one/' + id)
+  const {data} = await $authAdminHost.get('api/order/one/' + id)
   return data
 }
 
 export const fetchProducts = async (categoryId, name, page, limit, minPrice, maxPrice, inStock) => {
-  const {data} = await $host.get('api/product/all-admin', {params: {
+  const {data} = await $authAdminHost.get('api/product/all-admin', {params: {
     categoryId, name, page, limit, minPrice, maxPrice, inStock
   }})
   return data
 }
 
 export const fetchProductInfo = async (id) => {
-  const {data} = await $host.get('api/product/product/' + id)
+  const {data} = await $authAdminHost.get('api/product/product/' + id)
   return data
 }
 
@@ -111,7 +111,7 @@ export const createProduct = async (
     formData.append('imageGallery', image);
   })
 
-  const {data} = await $host.post('api/product', formData, {
+  const {data} = await $authAdminHost.post('api/product', formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     }
@@ -153,7 +153,7 @@ export const updateProduct = async (
     formData.append('imageGallery', image);
   })
 
-  const {data} = await $host.put('api/product', formData, {
+  const {data} = await $authAdminHost.put('api/product', formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     }
@@ -162,29 +162,29 @@ export const updateProduct = async (
 }
 
 export const fetchPromocodes = async (limit, page) => {
-  const {data} = await $host.get('api/promo/all', {params: {
+  const {data} = await $authAdminHost.get('api/promo/all', {params: {
     limit, page
   }})
   return data
 }
 
 export const fetchPromocodeInfo = async (promocode) => {
-  const {data} = await $host.get('api/promo/one/' + promocode)
+  const {data} = await $authAdminHost.get('api/promo/one/' + promocode)
   return data
 }
 
 export const createPromocode = async (promocode, percentDiscount, expirationDate) => {
-  const {data} = await $host.post('api/promo', {promocode, percentDiscount, expirationDate})
+  const {data} = await $authAdminHost.post('api/promo', {promocode, percentDiscount, expirationDate})
   return data
 }
 
 export const updatePromocode = async (id, promocode, percentDiscount, expirationDate) => {
-  const {data} = await $host.put('api/promo', {id, promocode, percentDiscount, expirationDate})
+  const {data} = await $authAdminHost.put('api/promo', {id, promocode, percentDiscount, expirationDate})
   return data
 }
 
 export const fetchCategories = async (limit, page) => {
-  const {data} = await $host.get('api/category/all-admin', {params: {
+  const {data} = await $authAdminHost.get('api/category/all-admin', {params: {
     limit, page
   }})
   return data
